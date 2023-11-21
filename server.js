e81ce4133f1585e0517677afbfe81187f7d8877c
Akinlabitobi <113988927+Akinlabitobi@users.noreply.github.com>
2023-11-21 16:38:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 const port = 5000
 app.use(cors(
     {
-        origin: ["http://localhost:5173"],
+        origin: ["https://harvestersibadan.abhub.com.ng/"],
         methods: ["POST", "GET", "PUT"],
         credentials: true
     }
